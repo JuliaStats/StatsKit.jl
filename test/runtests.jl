e0aad7c205e3ba8d306c99ca151accd9842660d0
Milan Bouchet-Valat <nalimilan@club.fr>
2018-11-20 17:52:52 +0100

    Use simpler but less correct solution

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
+# Check that package loads
 using Stats
 
-# Run all tests in the same process to check that they pass when all
-# packages are loaded at the same time
-for m in [Bootstrap, CategoricalArrays, Clustering, CSV, DataFrames,
-          Distances, Distributions, GLM, HypothesisTests, KernelDensity,
-          Loess, MultivariateStats, Statistics, StatsBase, TimeSeries]
-    println("Testing $m...")
-    include(joinpath(dirname(pathof(m)), "..", "test", "runtests.jl"))
-end
+# TODO: run all tests in the same process after 'using Stats'
+#       to check that they pass when all packages are loaded at the same time
+Pkg.test(["Bootstrap", "CategoricalArrays", "Clustering", "CSV", "DataFrames",
+          "Distances", "Distributions", "GLM", "HypothesisTests", KernelDensity",
+          "Loess", "MultivariateStats", "Statistics", "StatsBase", "TimeSeries"])
