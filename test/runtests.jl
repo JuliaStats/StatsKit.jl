7bb49cdd46da7b39ec13df5162071749baa65d27
Milan Bouchet-Valat <nalimilan@club.fr>
2018-11-20 18:03:23 +0100

    Never develop directly on GitHub

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,5 @@ using Pkg
 # TODO: run all tests in the same process after 'using Stats'
 #       to check that they pass when all packages are loaded at the same time
 Pkg.test(["Bootstrap", "CategoricalArrays", "Clustering", "CSV", "DataFrames",
-          "Distances", "Distributions", "GLM", "HypothesisTests", KernelDensity",
+          "Distances", "Distributions", "GLM", "HypothesisTests", "KernelDensity",
           "Loess", "MultivariateStats", "Statistics", "StatsBase", "TimeSeries"])
