3a0fb74bf74630759cd160019832dc62e8b43c96
Milan Bouchet-Valat <nalimilan@club.fr>
2021-02-22 09:51:19 +0100

    Improve tests
    Running tests for each package separately isn't very useful as they run their tests
    already, and it hits timeout given how long this takes.
    What we would ideally do is run them in the same process after loading StatsKit,
    but that's currently not possible.
    
    Testing that reexxported packages do not have conflicting identifiers seems more useful.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,21 @@
 # Check that package loads
 using StatsKit
 
-using Pkg
+using Test
 
 # TODO: run all tests in the same process after 'using StatsKit'
 #       to check that they pass when all packages are loaded at the same time
-Pkg.test(["Bootstrap", "CategoricalArrays", "Clustering", "CSV", "DataFrames",
-          "Distances", "Distributions", "GLM", "HypothesisTests", "KernelDensity",
-          "Loess", "MultivariateStats", "StatsBase", "ShiftedArrays", "TimeSeries"])
+#       This is not possible currently as Pkg.test starts a new environment
+
+# Check that reexported packages do not export different objects under the same name
+objects = Dict{Any, Symbol}()
+for m in [Bootstrap, CategoricalArrays, Clustering, CSV, DataFrames,
+          Distances, Distributions, GLM, HypothesisTests, KernelDensity,
+          Loess, MultivariateStats, MixedModels, StatsBase, ShiftedArrays, TimeSeries]
+    for name in names(m)
+        # Work around JuliaStats/StatsBase#663
+        name === :findat && continue
+        push!(objects, getproperty(m, name) => name)
+    end
+end
+@test allunique(values(objects))
