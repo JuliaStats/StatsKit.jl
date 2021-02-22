3dd416aa86da56e2206f9c231d5187c8d1518978
Milan Bouchet-Valat <nalimilan@club.fr>
2021-02-22 12:03:42 +0100

    Improvements

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,30 @@ for m in [Bootstrap, CategoricalArrays, Clustering, CSV, DataFrames,
           Distances, Distributions, GLM, HypothesisTests, KernelDensity,
           Loess, MultivariateStats, MixedModels, StatsBase, ShiftedArrays, TimeSeries]
     for name in names(m)
-        # Work around JuliaStats/StatsBase#663
-        name === :findat && continue
+        # Work around undefined exports:
+        # JuliaStats/StatsBase#663,
+        # JuliaStats/Clustering.j#210,
+        # JuliaStats/GLM.jl#405,
+        # JuliaStats/StatsModels.jl#218,
+        # JuliaStats/MultivariateStats.jl#142,
+        # JuliaArrays/ShiftedArrays.jl#42,
+        # JuliaStats/TimeSeries.jl#487
+        name in (:findat, :kmeans_opts, :Gaussian, :LagTerm, :dropterm,
+                 :invsqrtm!, :wmean!, :reduce_vec, :mapreduce_vec, :find) && continue
         push!(objects, getproperty(m, name) => name)
     end
 end
-@test allunique(values(objects))
+
+exceptions = [:ci, :head, :lead, :predict, :predict, :rename!, :rename, :msd, :evaluate,
+              :rmsd, :ci, :rename!, :lag, :lag, :tail, :colwise, :colwise, :head, :meanad,
+              :msd, :transform, :tail, :rmsd, :rename, :transform, :evaluate, :lead]
+objects2 = filter(((k, v),) -> !(v in exceptions), objects)
+if !allunique(values(objects2))
+    for (k, v) in objects2
+        conflicts = filter(((k2, v2),) -> v == v2, objects2)
+        if length(conflicts) > 1
+            @show conflicts
+        end
+    end
+    error("Conflicting functions not in known exceptions found")
+end
